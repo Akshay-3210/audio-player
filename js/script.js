9828fe667f8732765624e71bca8f1aaa32fcd88d
Akshay-3210 <akshaysharma8630292910@gmail.com>
2025-07-18 00:28:49 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function formatSecondsToMinutes(seconds) {
 
 async function gsongs(folder) {
     currfolder = folder;
-    let a = await fetch(`https://github.com/Akshay-3210/audio-player/tree/main/songs/${folder}`);
+    let a = await fetch(`/songs/${folder}`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -75,7 +75,7 @@ const playmusic = (e, pause = false) => {
 }
 
 async function dsiplayalbums() {
-    let a = await fetch(`https://github.com/Akshay-3210/audio-player/tree/main/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -88,7 +88,7 @@ async function dsiplayalbums() {
         if (e.href.includes("\songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0];
             //meta data
-            let a = await fetch(`https://github.com/Akshay-3210/audio-player/tree/main/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);
             // console.log(a);
             let response = await a.json();
             // console.log(response);
@@ -100,7 +100,7 @@ async function dsiplayalbums() {
                                 <!-- Green Circle --><circle cx="50" cy="50" r="45" fill="#1ed760" /><!-- Black Triangle --><polygon points="40,30 70,50 40,70" fill="black" />
                             </svg>
                         </div>
-                        <img src=https://github.com/Akshay-3210/audio-player/tree/main/songs/${folder}/cover.jpg alt="">
+                        <img src=/songs/${folder}/cover.jpg alt="">
                         <h2>${response.title}</h2>
                         <p>${response.description}</p>
                     </div>
